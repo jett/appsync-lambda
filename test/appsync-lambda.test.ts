43ed7b36c3728ed00e6db8a43319da1440934739
Jett Gamboa <jettgamboa@devfactory.com>
2023-03-01 11:21:23 +0800

    Initial commit

---
+++ test/appsync-lambda.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AppsyncLambda from '../lib/appsync-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncLambda.AppsyncLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
